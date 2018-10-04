353bacbb4d2ef22c36aa4df48d4e2a01ae245f63
adammcpherson <aomcpherson@gmail.com>
2018-10-04 18:18:17 -0400

    added port to database

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ $capsule = new Capsule;
 $capsule->addConnection([
 	'driver' => 'mysql',
 	'host' => 'localhost',
+    'port' => '3306',
 	'database' => 'cart',
 	'username' => 'root',
 	'password' => '',
