0c991b2ccd12af522468428cd81a902e6cab4a5d
adammcpherson <aomcpherson@gmail.com>
2018-10-05 10:46:34 -0400

    removed port

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,6 @@ $capsule = new Capsule;
 $capsule->addConnection([
 	'driver' => 'mysql',
 	'host' => '127.0.0.1',
-    'port' => '3306',
 	'database' => 'cart',
 	'username' => 'root',
 	'password' => '',
